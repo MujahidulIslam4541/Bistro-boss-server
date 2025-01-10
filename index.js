e871cd2b5fd5860b3f4796ffc17b8fb9d970e93d
MujahidulIslam4541 <mirifat4541@gmail.com>
2025-01-10 22:16:01 +0600

    enu delete upadte and multifle change

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ async function run() {
       if (user) {
         admin = user?.role === "admin";
       }
-      res.send({admin});
+      res.send({ admin });
     });
 
     // user related api
@@ -95,6 +95,46 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+
+    app.post("/menu", async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+    });
+    app.delete("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
+   
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: (id) };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+
+
+    app.patch('/menu/:id',async(req,res)=>{
+      const item=req.body;
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const updatedDoc={
+        $set:{
+          name:item.name,
+          category:item.category,
+          price:item.price,
+          image:item.image,
+          recipe:item.recipe
+        }
+      }
+      const result=await menuCollection.updateOne(query,updatedDoc)
+      res.send(result)
+    
+    })
+
+
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
