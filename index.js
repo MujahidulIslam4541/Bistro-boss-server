90e25ae61c90ffbfeb800d1902aaadff5108c466
MujahidulIslam4541 <mirifat4541@gmail.com>
2025-01-26 22:13:51 +0600

    create vercel.json and some changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const menuCollection = client.db("BistroDb").collection("menu");
     const userCollection = client.db("BistroDb").collection("users");
     const reviewCollection = client.db("BistroDb").collection("review");
@@ -108,33 +108,30 @@ async function run() {
       const result = await menuCollection.deleteOne(query);
       res.send(result);
     });
-   
+
     app.get("/menu/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: (id) };
+      const query = { _id: id };
       const result = await menuCollection.findOne(query);
       res.send(result);
     });
 
-
-    app.patch('/menu/:id',async(req,res)=>{
-      const item=req.body;
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)}
-      const updatedDoc={
-        $set:{
-          name:item.name,
-          category:item.category,
-          price:item.price,
-          image:item.image,
-          recipe:item.recipe
-        }
-      }
-      const result=await menuCollection.updateOne(query,updatedDoc)
-      res.send(result)
-    
-    })
-
+    app.patch("/menu/:id", async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          image: item.image,
+          recipe: item.recipe,
+        },
+      };
+      const result = await menuCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
 
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
@@ -162,41 +159,57 @@ async function run() {
       res.send(result);
     });
 
-
     // payment method intent
-    app.post('/content-payment-intent',async(req,res)=>{
-      const {price}=req.body;
-      const amount=parseInt(price*100)
-      const paymentIntent=await stripe.paymentIntents.create({
-        amount:amount,
-        currency:'usd',
-        payment_method_types:['card']
+    app.post("/content-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
       });
-      res.send({clientSecret:paymentIntent.client_secret})
-    })
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
 
     // payment saved api
-    app.post('/payment',async(req,res)=>{
-      const payment=req.body;
-      const paymentResult=await paymentCollection.insertOne(payment)
-      const query={_id:{
-          $in:payment.cartId.map(id=>new ObjectId(id))
-      }}
-      const deleteResult=await cardCollection.deleteMany(query)
-      res.send({paymentResult,deleteResult})
-    })
+    app.post("/payment", async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+      const query = {
+        _id: {
+          $in: payment.cartId.map((id) => new ObjectId(id)),
+        },
+      };
+      const deleteResult = await cardCollection.deleteMany(query);
+      res.send({ paymentResult, deleteResult });
+    });
 
-    app.get('/payment/:email',async(req,res)=>{
-      const query={email:req.params.email};
-      const result=await paymentCollection.find(query).toArray()
-      res.send(result)
-    })
+    app.get("/payment/:email", async (req, res) => {
+      const query = { email: req.params.email };
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // state or analytics
+    app.get("/admin-stats", async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuitems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      // this is not the best way
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce(
+        (total, payment) => total + payment.price,
+        0
+      );
+      res.send({ users, menuitems, orders, revenue });
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
